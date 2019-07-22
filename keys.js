34ddf751a38c2074c14b9529e102988403faaf2b
George <Jc82200172@gmail.com>
2019-07-22 12:18:31 -0700

    6th commit

--- keys.js
+++ keys.js
@@ -1,12 +1,4 @@
 exports.spotify = {
-  id: 215dde8c54f44d9dbca3ab476a184415,
-  secret: 4f8e4c135e4c45a2a6a37ddf420f820d
+id: process.env.SPOTIFY_ID,
+secret: process.env.SPOTIFY_SECRET
 };
-
-spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-
-  console.log(data);
-});
