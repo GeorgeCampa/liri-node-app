d95bc1297ef02889b315c9c5a33c9adba84e95aa
George <Jc82200172@gmail.com>
2019-07-21 18:49:15 -0700

    5th commit

--- keys.js
+++ keys.js
@@ -1,4 +1,12 @@
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: 215dde8c54f44d9dbca3ab476a184415,
+  secret: 4f8e4c135e4c45a2a6a37ddf420f820d
 };
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+
+  console.log(data);
+});
--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
 var request = require('request');
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 
